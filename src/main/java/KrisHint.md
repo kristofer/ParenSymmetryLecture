3be69816d326dbfafa025286d50f1ced03ee7fd0
Kristofer Younger <kris@youngers.org>
2024-11-01 10:13:49 -0400

    yowza

---
+++ src/main/java/KrisHint.md
@@ -0,0 +1,148 @@
+
+## If we have...
+
+"()"  -> true
+"( ( ) )" -> true
+"(( (() ) ) )" -> true
+"(zipcode)" -> true
+
+"(" -> false
+")" -> false
+"(()" -> false
+"())" -> false
+"" -> false
+"zipcode" -> false
+")(" -> false
+
+"(((((((((())))))))))" -> true
+"(())()()()()((()))()()()" -> true
+"((( () () () )))" -> true
+
+")((((((((())))))))))" -> false
+"(())()()()()(()))()()()" -> false
+"((( () ( () )))" -> false
+
+
+// STEP ZERO
+// get the obvious stuff out of the way
+//
+// We know we will HAVE TO PROCESS each CHARACTER in
+// the input STRING s.
+// c is a single char from s
+//
+func is-balanced1(s) {
+
+    for each c in string-s
+        if '(' == c, doSomething()
+        if ')' == c, doSomethingElse()
+
+    if the-string-is-balanced return true
+    // else
+    return false
+}
+// NOW, what ideas for doSomething() and doSomethingElse() ??
+
+
+
+
+// count ( and ) and look for equal number
+// ")(" -> false
+// "" -> false
+func is-balanced1(s) {
+    parens = 0
+    thesis = 0
+
+    for each c in string-s
+        if '(' == c, parens++
+        if ')' == c, thesis++
+
+    if parens == thesis return true
+    else return false
+}
+
+// but what's the problem? ==> ")(" is false (but it would say true)
+// also "" ==> true (but it's not)
+
+// What other possibilities?
+
+s = "()foo()bar"
+remove nondelimiter => "()()"
+
+
+// count ( and ) and look for equal number
+func is-balanced2(s) {
+    parens = 0
+    thesis = 0
+
+    for each c in string-s
+        if '(' == c, parens++
+        if ')' == c, thesis++
+        if thesis > parens return false
+
+    if parens == thesis return true
+    else return false
+}
+
+// now at least we'd catch ")(" but not ""
+
+
+
+// count ( and ) and look for equal number
+func is-balanced2(s) {
+
+    if s == "" return false // what's known as a
+                            // "short-circuit" case
+    if s.isEmpty() return false // what's known as a
+                            // "short-circuit" case
+
+    parens = 0
+    thesis = 0
+
+    for each c in string-s
+        if '(' == c, parens++
+        if ')' == c, thesis++
+        if thesis > parens return false
+
+    if parens == thesis return true
+    
+    return false
+}
+
+// an alternative is to just add up the parens, and subtract the thesis
+// if empty string, false
+//    if negative, false (inside)
+// if positive, false
+// if zero, true
+
+
+```java
+//parensymmetry help
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadFileLineByLineUsingBufferedReader {
+
+	public static void main(String[] args) {
+		BufferedReader reader;
+
+		try {
+			reader = new BufferedReader(new FileReader("sample.txt"));
+			String line = reader.readLine();
+
+			while (line != null) {
+				System.out.println(line); // process it somehow using isBalanced
+				// read next line
+				line = reader.readLine();
+			}
+
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
+```
+
---
+++ src/main/java/ParenSymmetry.java
@@ -0,0 +1,231 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.function.Function;
+
+public class ParenSymmetry {
+
+    public static void main(String[] args) {
+        ParenSymmetry ps = new ParenSymmetry();
+        //ps.runDetailedExamples();
+        ps.runFileExamples();
+    }
+
+    /* 
+     * This function takes a string and returns a boolean
+     * If the number of parens equals the number of thesis, return true
+     * else return false
+     * if at any point the number of either parens or thesis is less than 0, return false
+     */
+
+     // Number 1
+    // public Boolean isBalanced(String s) {
+    //     Boolean result = false;
+    //     if (s.isEmpty()) {
+    //         return result;
+    //     }
+    //     int parens = 0;
+    //     int thesis = 0;
+
+    //     char[] c = s.toCharArray();
+
+    //     for (int i = 0; i < c.length; i++) {
+    //         if (c[i] == '(') {
+    //             parens++;
+    //         } else if (c[i] == ')') {
+    //             thesis++;
+    //         }
+    //         if (thesis > parens) {
+    //             return false;
+    //         }
+    //     }
+    //     if (parens == thesis) {
+    //         result = true;
+    //     }
+    //     return result;
+    // }
+
+    // Number 2
+    // public Boolean isBalanced(String s) {
+    //     if (s.isEmpty()) return false;
+    //     char[] c = s.toCharArray();
+    //     int parens = 0;
+    //     int thesis = 0;
+    //     for (int i = 0; i < c.length; i++) {
+    //         if (c[i] == '(') {
+    //             parens++;
+    //         } else if (c[i] == ')') {
+    //             thesis++;
+    //         }
+    //     }
+    //     if (parens - thesis == 0) {
+    //         return true;
+    //     }
+    //     return false;
+    // }
+
+    // Number 3
+    public Boolean isBalanced(String s) {
+        // if the string is empty, return false
+        if (s.isEmpty()) {
+            return false;
+        }
+        int parenBalance = 0;
+        for (char c : s.toCharArray()) {
+            if (c != '(' && c != ')') {
+                continue;
+            }
+            if (c == '(') {
+                parenBalance++;
+            } else if (c == ')') {
+                parenBalance--;
+            }
+            if (parenBalance < 0) {
+                return false;
+            }
+        }
+        if (parenBalance == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    // Number 4
+    // public Boolean isBalanced(String s) {
+
+    //     if (s.isEmpty()) return false;
+        
+    //     char[] c = s.toCharArray();
+    //     int parens = 0;
+    //     for (int i = 0; i < c.length; i++) {
+    //         if (c[i] == '(') {
+    //             parens++;
+    //         } else if (c[i] == ')') {
+    //             parens--;
+    //         }
+    //     }
+    //     if (parens == 0) {
+    //         return true;
+    //     }
+    //     return false;
+    // }
+    
+    // Number 5
+    // public Boolean isBalanced(String s) {
+    //     if (s.isEmpty()) return false;
+    //     int parens = 0;
+    //     for (char c : s.toCharArray()) {
+    //         if (c == '(') parens++;
+    //         if (c == ')') parens--;
+    //     }
+    //     if (parens == 0) return true;
+    //     return false;
+    // }
+
+
+    void checkFile(String filename) {
+        // open file named filename
+
+        // for each line in the file
+            // read the line
+            // print whether or not the line's parenthesis are balanced
+
+        // CLOSE the file
+
+        BufferedReader reader;
+
+		try {
+			reader = new BufferedReader(new FileReader(filename));
+			String line = reader.readLine();
+
+			while (line != null) {
+				//System.out.println(line);
+
+                printResult2(this.isBalanced(line), line);
+				
+                // read next line
+				line = reader.readLine();
+			}
+
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+    }
+
+    public void runExamples() {
+        Boolean b0 = this.isBalanced("()");
+        System.out.println(b0);
+
+        Boolean b1 = this.isBalanced("())");
+        System.out.println(b1);
+
+        Boolean b2 = this.isBalanced("(()");
+        System.out.println(b2);
+
+        Boolean b3 = this.isBalanced("()grand()illusion");
+        System.out.println(b3);
+    }
+
+    public void runFileExamples() {
+        System.out.println(">> Running file examples");
+        System.out.println(System.getProperty("user.dir"));
+        System.out.println(">> TestStrings0.txt");
+        this.checkFile("TestStrings0.txt");
+        System.out.println(">> TestStrings1.txt");
+        this.checkFile("TestStrings1.txt");
+    }
+
+    public void runDetailedExamples() {
+
+        /*
+         * Notice the use of `this`
+         */
+        Boolean b0 = this.isBalanced("()");
+        printResult(b0, true, "()");
+
+        System.out.println("\nFalses\n");
+        String[] falseStrings = {"(", "((", ")", "", "(()())((())))", ")(", "))(("};
+        Boolean falses = true;
+        for (String strToTest : falseStrings) {
+            falses = this.isBalanced(strToTest);
+            printResult(falses, false, strToTest);
+        }
+
+        System.out.println("\nTrues\n");
+
+        String[] trueStrings = {"()", "(())", "(((())))", "(()())((()))", "(   )", "( )", "( () ( ) )"};
+        Boolean trues = false;
+        for (String strToTest : trueStrings) {
+            trues = this.isBalanced(strToTest);
+            printResult(trues, true, strToTest);
+        }
+
+    }
+
+    private static void printResult(Boolean b0, boolean b, String input) {
+        System.out.print(b + ": " + input + ", ");
+        if (b0 == null) {
+            System.out.println("Null Failure");
+            return;
+        }
+        if (b0 == b) {
+            System.out.println(" Success");
+        } else {
+            System.out.println(" Failure");
+        }
+    }
+    private static void printResult2(Boolean b0, String input) {
+        System.out.print("??" + ": " + input + ", ");
+        if (b0 == null) {
+            System.out.println("Null Failure");
+            return;
+        }
+        if (b0) {
+            System.out.println(" Success");
+        } else {
+            System.out.println(" Failure");
+        }
+    }
+}
